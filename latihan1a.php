2bf79ac9a936fa633f5ac28c4ff7b51e629790aa
Lenovo <mikajatnika7@gmail.com>
2021-03-11 12:18:10 +0700

    Latihan Modul 1 Struktur control

---
+++ latihan1a.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Mika Quarta Mulya Jatnika
+193040067
+https://github.com/MIkaQuartaMJ/pw2021_193040067
+Pertemuan 1 - Kamis 8.00
+*/
+
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i, $j) <br>";
+    }
+}
