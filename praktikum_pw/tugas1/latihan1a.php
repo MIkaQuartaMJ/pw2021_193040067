3dccc31d8472f9511652206346076ba299cd78e7
Lenovo <mikajatnika7@gmail.com>
2021-03-11 10:57:10 +0700

    menyelesaikan latihan1a

---
+++ praktikum_pw/tugas1/latihan1a.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Mika Quarta Mulya Jatnika
+193040067
+https://github.com/MIkaQuartaMJ/pw2021_193040067
+Praktikum Pertemuan 1 
+*/
+?>
+<?php
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "Ini pengulangan Ke-$i,$j</br>";
+    }
+}
+?>
